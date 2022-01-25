0f4baeb61aee8c2c0b71ec7a965d54b98d24ab89
djcaps <92235072+djcaps@users.noreply.github.com>
2022-01-24 20:09:13 -0600

    este es el correcto

--- paper.js
+++ paper.js
@@ -1,5 +1,5 @@
 class paper{
-    constructor(x,y){
+    constructor(x,y,r){
             var options = {
                isStatic:false,
                'restitution':0.3,
@@ -13,19 +13,20 @@ class paper{
             };
             this.x=x;
             this.y=y;
+            this.r=r;
+            this.image  = loadImage("paper.png")           
             this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
-            this.image  = loadImage("paper.png")
+           
             World.add(world, this.body);
     
    
         }
 display(){
-    var angle = this.body.angle;
+    var paperpos=this.body.position;
     push();
-    translate(this.body.position.x, this.body.position.y);
-    rotate(angle);
+    translate(paperpos.x, paperpos.y);
     imageMode(CENTER);
-    image(this.image, 0, 0, this.width, this.height);
+    image(this.image, 0,0,this.r, this.r);
     pop();
 }
     }
